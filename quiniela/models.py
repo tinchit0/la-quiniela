6ee29d4fdf51342e901d0e2f2fcff3209f07a5d1
Rjaume <68929395+Rjaume@users.noreply.github.com>
2023-01-17 12:24:29 +0100

    Implemento model al projecte

--- quiniela/models.py
+++ quiniela/models.py
@@ -1,15 +1,23 @@
 import pickle
-
+from sklearn.ensemble import RandomForestRegressor
 
 class QuinielaModel:
 
+    def __init__(self):
+        self.model = RandomForestRegressor(n_estimators = 100)
+
     def train(self, train_data):
-        # Do something here to train the model
-        pass
+        x_train = train_data[['home_team_num', 'away_team_num', 'home_team_rank', 'away_team_rank']]
+        y_train = train_data['winner']
+        self.model.fit(x_train,y_train)
+        
+
 
     def predict(self, predict_data):
         # Do something here to predict
-        return ["X" for _ in range(len(predict_data))]
+        x_predict = predict_data[['home_team_num', 'away_team_num', 'home_team_rank', 'away_team_rank']]
+        prediction = self.model.predict(x_predict).astype(int)
+        return prediction 
 
     @classmethod
     def load(cls, filename):
